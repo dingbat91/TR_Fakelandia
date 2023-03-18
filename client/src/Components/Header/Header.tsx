76c3b251f7a37ec9d29a41027532a680510367c6
Matthew Hanson <dingbat91@gmail.com>
2023-03-18 13:23:32 +0000

    adjustment to normalise

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../../globalCSS/normalize.css";
+import "../../normalize/normalize.css";
 import "./Header.css";
 
 export const Header: React.FC = () => {
