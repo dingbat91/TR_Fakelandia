acd31da412623a6905edebfe3fdd3a4b5052b4d2
Matthew Hanson <dingbat91@gmail.com>
2023-03-19 14:23:33 +0000

    Basic Confession Form

--- client/src/Components/Confessions/Confessions.tsx
+++ client/src/Components/Confessions/Confessions.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export const Confessions: React.FC = () => {
-	return <div>Confessions</div>;
+	return (
+		<div>
+			<form>
+				<label htmlFor='ConfessionSubject'>Subject</label>
+				<input name='ConfessionSubject' id='ConfessionSubject' type='text' />
+				<label htmlFor='Reason'>Reason for Contact</label>
+				<select name='Reason' id='Reason'>
+					<option value='Confession'>Confession</option>
+					<option value='Report'>Report</option>
+					<option value='Question'>Question</option>
+				</select>
+				<label htmlFor='ConfessionInfo'>Details</label>
+				<textarea name='ConfessionInfo' id='ConfessionInfo'></textarea>
+				<button>Submit</button>
+			</form>
+		</div>
+	);
 };
