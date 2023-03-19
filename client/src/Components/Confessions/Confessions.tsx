7cd37151e3394e8c296362902f691826f6e0b2a1
Matthew Hanson <dingbat91@gmail.com>
2023-03-19 16:39:09 +0000

    Rebuilt form with React-Hook-Form

--- client/src/Components/Confessions/Confessions.tsx
+++ client/src/Components/Confessions/Confessions.tsx
@@ -1,20 +1,54 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+
+enum ReasonEnum {
+	misdemeanour = "Misdemeanour",
+	venting = "Venting",
+}
+
+interface Inputs {
+	ConfessionSubject: string;
+	ConfessionReason: ReasonEnum;
+	ConfessionInfo: string;
+}
 
 export const Confessions: React.FC = () => {
+	const {
+		register,
+		handleSubmit,
+		getValues,
+		watch,
+		formState: { errors },
+	} = useForm<Inputs>();
+
+	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
 	return (
 		<div>
-			<form>
-				<label htmlFor='ConfessionSubject'>Subject</label>
-				<input name='ConfessionSubject' id='ConfessionSubject' type='text' />
-				<label htmlFor='Reason'>Reason for Contact</label>
-				<select name='Reason' id='Reason'>
-					<option value='Confession'>Confession</option>
-					<option value='Report'>Report</option>
-					<option value='Question'>Question</option>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input
+					{...register("ConfessionSubject", {
+						required: "Please Input a message here",
+					})}
+				/>
+				<ErrorMessage
+					errors={errors}
+					name='ConfessionSubject'
+					render={({ message }) => <>{message}</>}
+				/>
+				<select {...register("ConfessionReason")}>
+					<option value='Misdemeanour'>Misdemeanour</option>
+					<option value='Venting'>Venting</option>
 				</select>
-				<label htmlFor='ConfessionInfo'>Details</label>
-				<textarea name='ConfessionInfo' id='ConfessionInfo'></textarea>
-				<button>Submit</button>
+				<textarea
+					{...register("ConfessionInfo", {
+						required: true,
+						minLength: 5,
+						maxLength: 2000,
+					})}
+				/>
+				<input type='submit' />
 			</form>
 		</div>
 	);
